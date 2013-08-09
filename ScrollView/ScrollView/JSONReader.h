f44435511f2abb70cbf8a145c6da427624d1bfc6
genxium <genxium@hotmail.com>
2013-08-09 23:07:35 +0800

    Updated ScrollView:
    Added JSONReader class

---
+++ ScrollView/ScrollView/JSONReader.h
@@ -0,0 +1,54 @@
+//
+//  JSONReader.h
+//  Puzzle
+//
+//  Created by Tact Sky on 5/8/13.
+//
+//
+
+#ifndef __Puzzle__JSONReader__
+#define __Puzzle__JSONReader__
+
+#include <iostream>
+#include <string>
+#include "cJSON.h"
+#include "cocos2d.h"
+#include "HttpClient.h"
+#include "HttpRequest.h"
+#include "HttpResponse.h"
+#include "time.h"
+using namespace std;
+using namespace cocos2d;
+using namespace extension;
+
+class JSONReader : public CCObject
+{
+    
+public:
+    JSONReader();
+    virtual ~JSONReader();
+    static JSONReader* create(CCObject* pTarget, SEL_CallFuncO pSelector);
+    bool init(CCObject* pTarget, SEL_CallFuncO pSelector);
+    
+public:
+    void downloadJSON();
+    bool isListFileUpToDate();
+    bool updateListFile();
+    
+private:
+    // callback functions
+    void onJSONDownloadFinished(CCNode*, void*);
+    
+private:
+    
+    string m_feed;
+    string m_listFilePath;
+    string m_settingFilePath;
+    
+    CCObject*          m_pTarget;        /// callback target of pSelector function
+    SEL_CallFuncO     m_pSelector;      /// callback function, e.g. MyLayer::onHttpResponse(CCObject *sender, void *data)
+    string m_res;
+    
+};
+
+#endif /* defined(__Puzzle__JSONReader__) */
