a1f445d93bb155513e1bd6ca90acc77e9a949e2b
genxium <genxium@hotmail.com>
2013-09-05 01:29:51 +0800

    Updated makefiles.

--- HelloCococ2dx/proj.android/jni/Android.mk
+++ HelloCococ2dx/proj.android/jni/Android.mk
@@ -6,11 +6,27 @@ LOCAL_MODULE := game_shared
 
 LOCAL_MODULE_FILENAME := libgame
 
+DIR_PATH_CLASSES:= $(LOCAL_PATH)/../../Classes
+
+CPP_FILES:= $(shell find $(DIR_PATH_CLASSES) -type f -name "*.cpp" -print)
+C_FILES:= $(shell find $(DIR_PATH_CLASSES) -type f -name "*.c" -print)
+
+ALL_SRC_FILES:= $(CPP_FILES)
+ALL_SRC_FILES+= $(C_FILES)
+
+ALL_INCLUDE_FOLDERS:= $(shell find $(DIR_PATH_CLASSES) -type d -print)
+
+TRANSLATED_SRC_FILES:= $(subst $(LOCAL_PATH)/,,$(ALL_SRC_FILES))
+
+$(warning $(TRANSLATED_SRC_FILES))
+
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
                    ../../Classes/HelloWorldScene.cpp
-                   
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes                   
+
+LOCAL_SRC_FILES:= $(TRANSLATED_SRC_FILESi) 
+                  
+LOCAL_C_INCLUDES := $(ALL_INCLUDE_FOLDERS)
 
 LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static cocosdenshion_static cocos_extension_static
             
--- HelloCococ2dx/proj.android/obj/local/armeabi/objs/cocos2dx_static/support/ccUtils.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/support/ccUtils.o: \
- /Users/user/cocos2d-2.1rc0-x-2.1.3/HelloCococ2dx/proj.android/../../cocos2dx/support/ccUtils.cpp \
- /Users/user/cocos2d-2.1rc0-x-2.1.3/HelloCococ2dx/proj.android/../../cocos2dx/support/ccUtils.h
-
-/Users/user/cocos2d-2.1rc0-x-2.1.3/HelloCococ2dx/proj.android/../../cocos2dx/support/ccUtils.h:
