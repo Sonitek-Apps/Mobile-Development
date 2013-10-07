d7b9efb5e5a95b6ebd042aedbda49c3c1fe02527
genxium <genxium@hotmail.com>
2013-10-08 01:29:56 +0800

    Updated ScrollTest Project.
    1. Updated CCHttpClientEx to single (extra) thread version.
    2. Create CCHttpClientExMultiThreading class.

---
+++ ScrollTest/ScrollTest/Classes/Constants.h
@@ -0,0 +1,26 @@
+//
+//  Constants.h
+//  ScrollTest
+//
+//  Created by Wing on 5/9/13.
+//
+//
+
+#ifndef ScrollTest_Constants_h
+#define ScrollTest_Constants_h
+
+#define kConnnectionTimeout 15
+#define kReadTimeout 60
+#define kFeed "http://www.tactsky.com/ws/list/?view=list&from=tomlinsonphoto&type=latest"
+#define kFeedListPath CCFileUtils::sharedFileUtils()->getWritablePath()+"urlList"
+#define kSettingPath CCFileUtils::sharedFileUtils()->getWritablePath()+"setting"
+#define kDefaultBufferSize 1024
+
+#define kXmlKeyNumberOfLocalImages "numberOfLocalImages"
+#define kActiveIndicatorTips "Processing..."
+#define kSharedSaveFile CCUserDefault::sharedUserDefault()
+#define kNameMapPrefix "$%$#$-"
+#define kHttpPreix "http"
+#define kDownloadedPhotosListPath (CCFileUtils::sharedFileUtils()->getWritablePath().append("downloadedPhotosList")).c_str()
+
+#endif
---
+++ ScrollTest/ScrollTest/android/release_gen.sh
@@ -0,0 +1,1 @@
+ant -f ./build.xml clean release -Dsdk.dir=$ANDROID_SDK_ROOT -Dkey.store=/Users/user/my-release-key.keystore -Dkey.alias=first-release-key
